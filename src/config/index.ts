180b2455d00bb396201e5ef013cb6eda401618c0
Sawarz <sawarz22@gmail.com>
2023-11-05 18:24:39 +0100

    test: Change production url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://fun-comm-lbprs1a7w-sawarz.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'https://fun-comm.vercel.app';
