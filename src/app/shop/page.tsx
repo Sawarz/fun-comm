e1eb5601ff8fe62627b0b4cf8d792101292e0a78
Sawarz <sawarz22@gmail.com>
2023-11-05 12:08:56 +0100

    fix: Fix type in function

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,11 +3,13 @@ import axios from "axios";
 import { server } from "@/src/config/index";
 import ProductList from "@/src/components/productlist/ProductList";
 
+import styles from "./page.module.css";
+
 export default async function Shop() {
 	const { data: products } = await axios.get(`${server}/api/getProducts`);
 
 	return (
-		<div>
+		<div className={styles.shop}>
 			<ProductList products={products} />
 		</div>
 	);
