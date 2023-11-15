a992de8ad97d9e50c3eb7c96c02dbf1f48846a29
Sawarz <sawarz22@gmail.com>
2023-11-15 19:10:08 +0100

    fix: Remove redundant testing code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { prisma } from "@/src/utils/db";
 
 export default async function home() {
-	const users = await prisma.user.findMany();
-
 	return (
 		<div>
-			<div>{JSON.stringify(users)}</div>
+			<div>Home</div>
 		</div>
 	);
 }
