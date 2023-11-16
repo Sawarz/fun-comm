c2b096b2a9d706b0387729bb9d8e821c2ce5ae01
Tomasz Sawarzynski <sawarz22@gmail.com>
2023-11-16 12:37:35 +0100

    feat: Add uuid to product creation

--- src/app/api/addProduct/route.ts
+++ src/app/api/addProduct/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 	const data = await request.json();
 
-	const test = await prisma.product.create({ data: data });
+	const dbResponse = await prisma.product.create({ data: data });
 
-	return NextResponse.json(data);
+	return NextResponse.json(dbResponse);
 }
