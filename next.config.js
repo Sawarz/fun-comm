546894b5e1ec958a7d436fbde7d391cf788f65db
Tomasz Sawarzynski <sawarz22@gmail.com>
2023-11-16 11:28:28 +0100

    feat: Add firebase as image source

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
     sassOptions: {
         includePaths: ['./src'],
         prependData: `@import "~@styles/main.scss";`,
-    }
+    },
+    images: {
+        domains: ['firebasestorage.googleapis.com'],
+    },
 }
 
 module.exports = nextConfig
